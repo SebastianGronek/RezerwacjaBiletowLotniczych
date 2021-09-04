a8ede7d99d694f4473cb3726ce183b4574b77067
Sebastian Gronek <groneks.a@gmail.com>
2021-09-04 10:28:05 +0200

    First commit

---
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/RezerwacjaBiletowLotniczychApplication.java
@@ -0,0 +1,13 @@
+package JAVAwwa30.RezerwacjaBiletowLotniczych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerwacjaBiletowLotniczychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezerwacjaBiletowLotniczychApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JAVAwwa30/RezerwacjaBiletowLotniczych/RezerwacjaBiletowLotniczychApplicationTests.java
@@ -0,0 +1,13 @@
+package JAVAwwa30.RezerwacjaBiletowLotniczych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezerwacjaBiletowLotniczychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
