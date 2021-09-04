8d0762894a929d1712f5d93e98536f4f0374b686
Sebastian Gronek <groneks.a@gmail.com>
2021-09-04 15:28:50 +0200

    added initial data to database with data.sql

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/model/Flight.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/model/Flight.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +16,7 @@ import javax.persistence.Id;
 public class Flight {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String startingLocalization;
     private String destination;
