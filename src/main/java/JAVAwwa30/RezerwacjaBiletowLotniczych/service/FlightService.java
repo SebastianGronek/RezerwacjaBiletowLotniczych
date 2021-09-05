8cbed520a9d14863b06188b78ee54e12431e2a5a
Sebastian Gronek <groneks.a@gmail.com>
2021-09-05 11:20:51 +0200

    added FlightService with methods getFlightsFromOneDestinationToAnother and getFlightsFromOneDestinationToAnotherAfterDate

---
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/service/FlightService.java
@@ -0,0 +1,26 @@
+package JAVAwwa30.RezerwacjaBiletowLotniczych.service;
+
+import JAVAwwa30.RezerwacjaBiletowLotniczych.Repository.JPARepository;
+import JAVAwwa30.RezerwacjaBiletowLotniczych.model.Flight;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FlightService {
+    @Autowired
+    JPARepository jpaRepository;
+
+    public List<Flight> getFlightsFromOneDestinationToAnother(String startingLocalization, String destination) {
+        return jpaRepository.findFlightByStartingLocationAndDestination(startingLocalization, destination);
+    }
+
+
+    public List<Flight> getFlightsFromOneDestinationToAnotherAfterDate(String startingLocalization, String destination, String departureTime) {
+        LocalDateTime dateOfFlight = LocalDateTime.parse(departureTime);
+        return getFlightsFromOneDestinationToAnother(startingLocalization, destination).stream().filter(flight -> flight.getDateOfFlight().isAfter(dateOfFlight)).collect(Collectors.toList());
+    }
+}
