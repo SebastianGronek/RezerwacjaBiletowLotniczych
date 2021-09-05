6256c189caf1f87e4a95f9f2b2a3aef766969bc2
Sebastian <sebekk7@gmail.com>
2021-09-05 14:07:46 +0200

    add UserController, UserControllerTest

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/model/User.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/model/User.java
@@ -1,5 +1,9 @@
 package JAVAwwa30.RezerwacjaBiletowLotniczych.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,7 +13,10 @@ import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name="users")
+@Data
 public class User {
 
     @Id
