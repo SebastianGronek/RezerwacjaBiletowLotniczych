ae53bf6ef755c2ff92713e23d8f2e6ff11192034
Sebastian Gronek <groneks.a@gmail.com>
2021-09-05 10:10:11 +0200

    Searching flight by starting location and destination

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO flight (starting_localization, destination, duration_of_flight) VALUES ('Barcelona', 'London', '7');
-INSERT INTO flight (starting_localization, destination, duration_of_flight) VALUES ('Barcelona', 'London', '12');
-INSERT INTO flight (starting_localization, destination, duration_of_flight) VALUES ('Barcelona', 'Munich', '4');
-INSERT INTO flight (starting_localization, destination, duration_of_flight) VALUES ('Madrid', 'Lisbon', '2');
-INSERT INTO flight (starting_localization, destination, duration_of_flight) VALUES ('London', 'Oslo', '3');
+INSERT INTO flight (starting_location, destination, duration_of_flight) VALUES ('Barcelona', 'London', '7');
+INSERT INTO flight (starting_location, destination, duration_of_flight) VALUES ('Barcelona', 'London', '12');
+INSERT INTO flight (starting_location, destination, duration_of_flight) VALUES ('Barcelona', 'Munich', '4');
+INSERT INTO flight (starting_location, destination, duration_of_flight) VALUES ('Madrid', 'Lisbon', '2');
+INSERT INTO flight (starting_location, destination, duration_of_flight) VALUES ('London', 'Oslo', '3');
