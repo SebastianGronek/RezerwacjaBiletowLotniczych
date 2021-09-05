4ce6cc1e1f0ea195888350d42a30c05025a9986f
Sebastian <sebastian@pop-os.localdomain>
2021-09-05 12:08:57 +0200

    adding @ManyToMany annotation to lists in User and Flight

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/model/User.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/model/User.java
@@ -3,6 +3,7 @@ package JAVAwwa30.RezerwacjaBiletowLotniczych.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import java.util.List;
 
@@ -16,19 +17,7 @@ public class User {
     private Long userId;
     private String userFirstName;
     private String userName;
+    @ManyToMany
     private List <Flight> ticketList;
-    private List <Flight> archivalTicketList;
-
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId=" + userId +
-                ", userFirstName='" + userFirstName + '\'' +
-                ", userName='" + userName + '\'' +
-                ", ticketList='" + ticketList + '\'' +
-                ", archivalTicketList='" + archivalTicketList + '\'' +
-                '}';
-    }
 
 }
