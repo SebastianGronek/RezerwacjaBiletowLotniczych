d55e07940ba5dd52bac897d7eac31825a691094b
Sebastian <sebastian@pop-os.localdomain>
2021-09-06 10:36:35 +0200

    FlightController: deleting one of the GetMapping and instead using if in the other

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/controller/FlightController.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/controller/FlightController.java
@@ -4,7 +4,6 @@ import JAVAwwa30.RezerwacjaBiletowLotniczych.model.Flight;
 import JAVAwwa30.RezerwacjaBiletowLotniczych.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
