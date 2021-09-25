4a31f799cbaee306ad35947a769feb179969148d
Sebastian Gronek <groneks.a@gmail.com>
2021-09-25 12:09:28 +0200

    adding allUsers get endpoint

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/controller/UserController.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/controller/UserController.java
@@ -4,6 +4,7 @@ import JAVAwwa30.RezerwacjaBiletowLotniczych.Repository.UserRepository;
 import JAVAwwa30.RezerwacjaBiletowLotniczych.model.Flight;
 import JAVAwwa30.RezerwacjaBiletowLotniczych.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,13 @@ public class UserController {
         return userRepository.save(user);
     }
 
-    @PostMapping("/createUser")
+   /* @PostMapping("/createUser")
     public User createUser(String userFirstName, String userName) {
         return addUser(new User(userFirstName, userName));
+    }*/
+
+    @GetMapping("/allUsers")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
