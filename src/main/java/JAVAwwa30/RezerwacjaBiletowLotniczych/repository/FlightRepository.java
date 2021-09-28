8f44ad5478182e77710b26f5202c0a35d1deaa8c
Sebastian Gronek <groneks.a@gmail.com>
2021-09-28 22:36:12 +0200

    added DISTINCT keyword to FLightRepository

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/repository/FlightRepository.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/repository/FlightRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 public interface FlightRepository extends JpaRepository<Flight, Long> {
     List<Flight> findFlightByStartingLocationAndDestination(String startingLocation, String destination);
 
-    @Query("SELECT startingLocation FROM Flight")
+    @Query("SELECT DISTINCT startingLocation FROM Flight")
     List<String> findDistinctStartingLocation();
 
-    @Query("SELECT destination FROM Flight")
+    @Query("SELECT DISTINCT destination FROM Flight")
     List<String> findDistinctDestination();
 
 }
