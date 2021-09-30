3f2f144545c647c2149b692cc8d239e5dffb0da8
Sebastian <sebastian@pop-os.localdomain>
2021-09-30 23:05:06 +0200

    updated user model with login field, changed user service methods and controllers

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/repository/UserRepository.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/repository/UserRepository.java
@@ -2,6 +2,13 @@ package JAVAwwa30.RezerwacjaBiletowLotniczych.repository;
 
 import JAVAwwa30.RezerwacjaBiletowLotniczych.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT login FROM users")
+    List<String> findLogin();
+
 }
