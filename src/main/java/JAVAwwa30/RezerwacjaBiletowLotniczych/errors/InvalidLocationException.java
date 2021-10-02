2150cdc81f6b1782c8c1b649ba0ea8c08384e93b
Sebastian <sebastian@pop-os.localdomain>
2021-10-02 12:17:20 +0200

    fixed error handling so the message given in constructor is send to frontend

--- src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/errors/InvalidLocationException.java
+++ src/main/java/JAVAwwa30/RezerwacjaBiletowLotniczych/errors/InvalidLocationException.java
@@ -3,7 +3,7 @@ package JAVAwwa30.RezerwacjaBiletowLotniczych.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Location not found")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class InvalidLocationException extends Exception{
     public InvalidLocationException(String message) {
         super(message);
